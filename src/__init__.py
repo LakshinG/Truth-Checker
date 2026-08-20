e9ac74efdf3f5ef7e08b0b4e01e46c124fb63824
LakshinG <lakshinganesha@gmail.com>
2026-08-20 14:24:52 -0400

    fix: remove ragas from init to fix module compatibility issue

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,11 @@
 from .ingestion import IngestionPipeline
 from .vector_store import VectorStoreManager
 from .critic import CriticAgent, create_research_graph
-from .evaluation import run_ragas_evaluation
+# from .evaluation import run_ragas_evaluation
 
 __all__ = [
     "IngestionPipeline",
     "VectorStoreManager",
     "CriticAgent",
     "create_research_graph",
-    "run_ragas_evaluation",
 ]
